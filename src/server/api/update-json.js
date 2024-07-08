d42377bdbab2f73513e26c89b9ac694d91e53479
Michael <michael@uplord.co.uk>
2024-07-08 23:25:03 +0100

    test

--- src/server/api/update-json.js
+++ src/server/api/update-json.js
@@ -7,25 +7,31 @@ import path from 'path';
 export default defineEventHandler(async (event) => {
   // Ensure that the request method is POST
   if (event.node.req.method === 'POST') {
+
+    let test = 0;
     try {
       // Get the 'file' query parameter
       const { file } = getQuery(event);
       console.log(1, file)
+      test = 1;
 
       // Define the path to the JSON file
       const jsonFilePath = path.resolve(process.cwd(), 'data', file);
 
       console.log(2, jsonFilePath)
+      test = 2;
 
       // Read and parse the request body
       const body = await readBody(event);
       
       console.log(3, body)
+      test = 3;
 
       // Write the updated data to the JSON file
       fs.writeFileSync(jsonFilePath, JSON.stringify(body, null, 2));
 
       console.log(4, 'writeFileSync')
+      test = 4;
 
       // Respond with a success message
       return {
@@ -38,6 +44,7 @@ export default defineEventHandler(async (event) => {
       return {
         success: false,
         message: 'Failed to update data',
+        test: test
       };
     }
   } else {
